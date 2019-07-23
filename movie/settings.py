6bd63b477b4676f85d919d89edf697f6fc623288
jackeating <45235574+jackeating@users.noreply.github.com>
2019-07-24 08:19:48 +0900

    Update settings.py

--- movie/settings.py
+++ movie/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/catalog/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
